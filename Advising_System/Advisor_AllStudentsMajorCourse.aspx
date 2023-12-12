64f9479561946eca1ff1cc9101110d9e12442667
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-13 01:46:24 +0200

    Advisor student major course

---
+++ Advising_System/Advisor_AllStudentsMajorCourse.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Advisor_AllStudentsMajorCourse.aspx.cs" Inherits="Advising_System.Advisor_AllStudentsMajorCourse" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Approve / Reject Course Requests</h2>
+         <div style="display: flex; align-items: center; justify-content: right; margin: 20px; resize: vertical;">
+            <asp:Button ID="BackHome" runat="server" OnClick="BackAdvisorHome" Text="Back" />
+         </div>
+        <div>
+            <asp:DropDownList ID="majors" runat="server" AppendDataBoundItems="True" AutoPostBack="True" OnSelectedIndexChanged="majors_SelectedIndexChanged" >
+            </asp:DropDownList>
+        </div>
+        <asp:GridView ID="StudentsWithMajor" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Advising_System/Advisor_AllStudentsMajorCourse.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Advising_System
+{
+    public partial class Advisor_AllStudentsMajorCourse : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) { return; }
+            string connectionStirng = WebConfigurationManager.ConnectionStrings["Advising_Team_13"].ToString();
+            SqlConnection connection = new SqlConnection(connectionStirng);
+
+            try
+            {
+                SqlCommand getMajors = new SqlCommand("SELECT DISTINCT(major) FROM Student", connection);
+                getMajors.CommandType = CommandType.Text;
+                connection.Open();
+
+                SqlDataReader reader = getMajors.ExecuteReader(CommandBehavior.CloseConnection);
+
+                majors.Items.Insert(0, new ListItem("Select a major", string.Empty));
+                majors.SelectedIndex = 0;
+                while (reader.Read())
+                {
+                    string data = reader.GetString(0);
+                    majors.Items.Add(new ListItem(data, data));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            finally { connection.Close(); }
+
+        }
+
+
+        protected void BackAdvisorHome(object sender, EventArgs e)
+        {
+            Response.Redirect("/AdvisorHome.aspx");
+        }
+
+        protected void majors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string major = majors.SelectedValue;
+            if (string.IsNullOrEmpty(major)) { return; }
+
+            string connectionStirng = WebConfigurationManager.ConnectionStrings["Advising_Team_13"].ToString();
+            SqlConnection connection = new SqlConnection(connectionStirng);
+
+            try
+            {
+                SqlCommand getStudents = new SqlCommand("Procedures_AdvisorViewAssignedStudents", connection);
+                getStudents.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+
+                getStudents.Parameters.AddWithValue("@AdvisorID", 8); // {Session["UserID"]}
+                getStudents.Parameters.AddWithValue("@major", major);
+                SqlDataReader reader = getStudents.ExecuteReader(CommandBehavior.CloseConnection);
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                StudentsWithMajor.DataSource = dt;
+                StudentsWithMajor.DataBind();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+            finally { connection.Close(); }
+        }
+    }
+}

87203ee2bab1f9b4aa0995bfbfd115bc55abb5cb
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-13 15:15:07 +0200

    dlete cvourse as v

--- Advising_System/Advisor_DeleteCourseGP.aspx.cs
+++ Advising_System/Advisor_DeleteCourseGP.aspx.cs
@@ -7,20 +7,16 @@ using System.Web;
 using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using static Advising_System.Admin_LinkStudentAdvisor;
-using System.Diagnostics;
-using System.Configuration;
 
 namespace Advising_System
 {
-    public partial class Advisor_AddCourseToGradPlan : System.Web.UI.Page
+    public partial class Advisor_DeleteCourseGP : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(IsPostBack) { return; }
+            if (IsPostBack) { return; }
             loadStudentList();
             loadCourseList();
-
         }
         protected void loadStudentList()
         {
@@ -28,7 +24,7 @@ namespace Advising_System
             SqlConnection connection = new SqlConnection(connectionStirng);
             try
             {
-                SqlCommand AllStudents = new SqlCommand($"SELECT student_id, CONCAT(student_id, ' ', f_name, ' ', l_name) AS 'All' " +
+                SqlCommand AllStudents = new SqlCommand($"SELECT student_id, CONCAT(student_id, '-', f_name, ' ', l_name) AS 'All' " +
                     $"FROM Student WHERE advisor_id = 8\r\n", connection); // {Session["UserID"]} put in input of fn
                 AllStudents.CommandType = CommandType.Text;
                 connection.Open();
@@ -62,7 +58,7 @@ namespace Advising_System
             SqlConnection connection = new SqlConnection(connectionStirng);
             try
             {
-                SqlCommand AllCourses = new SqlCommand("SELECT DISTINCT(name) FROM Course\r\n", connection); // {Session["UserID"]} put in input of fn
+                SqlCommand AllCourses = new SqlCommand("SELECT course_id, CONCAT(course_id, '-', name) AS 'All' FROM Course\r\n", connection); // {Session["UserID"]} put in input of fn
                 AllCourses.CommandType = CommandType.Text;
                 connection.Open();
 
@@ -71,13 +67,13 @@ namespace Advising_System
                 DataTable dt = new DataTable();
                 dt.Load(reader);
 
-                CourseName.DataSource = dt;
-                CourseName.DataTextField = "name";
-                CourseName.DataValueField = "name";
-                CourseName.DataBind();
+                CourseIDs.DataSource = dt;
+                CourseIDs.DataTextField = "All";
+                CourseIDs.DataValueField = "course_id";
+                CourseIDs.DataBind();
 
-                CourseName.Items.Insert(0, new ListItem("Select a Course", string.Empty));
-                CourseName.SelectedIndex = 0;
+                CourseIDs.Items.Insert(0, new ListItem("Select a Course", string.Empty));
+                CourseIDs.SelectedIndex = 0;
 
                 reader.Close();
             }
@@ -89,37 +85,43 @@ namespace Advising_System
             }
             finally { connection.Close(); }
         }
+        protected void BackAdvisorHome(object sender, EventArgs e)
+        {
+            Response.Redirect("/AdvisorHome.aspx");
+        }
 
-        protected void AddCourse_Click(object sender, EventArgs e)
+        protected void DeleteCourse_Click(object sender, EventArgs e)
         {
             Message.Visible = false;
             string stID = StudentID.SelectedValue;
-            string semCode = SemesterCode.Text;
-            string cName = CourseName.SelectedValue;
+            string semCode = semesterCode.Text;
+            string cName = CourseIDs.SelectedValue;
 
-            if(string.IsNullOrEmpty(stID) || string.IsNullOrEmpty(semCode)
-                || string.IsNullOrEmpty(cName)) 
-            { 
+            if (string.IsNullOrEmpty(stID) || string.IsNullOrEmpty(semCode)
+                || string.IsNullOrEmpty(cName))
+            {
                 Message.ForeColor = System.Drawing.Color.Red;
                 Message.Visible = true;
                 Message.Text = "Invalid Input";
                 return;
             }
+            int cID = Int32.Parse(cName);
 
             string connectionStirng = WebConfigurationManager.ConnectionStrings["Advising_Team_13"].ToString();
             SqlConnection connection = new SqlConnection(connectionStirng);
             try
             {
-                SqlCommand InsertGradPlan = new SqlCommand("Procedures_AdvisorAddCourseGP", connection); // {Session["UserID"]} put in input of fn
-                InsertGradPlan.CommandType = CommandType.StoredProcedure;
+                SqlCommand DeleteCourseGP = new SqlCommand("Procedures_AdvisorDeleteFromGP", connection); // {Session["UserID"]} put in input of fn
+                DeleteCourseGP.CommandType = CommandType.StoredProcedure;
                 connection.Open();
 
-                InsertGradPlan.Parameters.AddWithValue("@student_Id", stID);
-                InsertGradPlan.Parameters.AddWithValue("@Semester_code", semCode);
-                InsertGradPlan.Parameters.AddWithValue("@course_name", cName);
+                DeleteCourseGP.Parameters.AddWithValue("@studentID", stID);
+                DeleteCourseGP.Parameters.AddWithValue("@semesterCode", semCode);
+                DeleteCourseGP.Parameters.AddWithValue("@courseID", cID);
 
-                int nRowsAffected = InsertGradPlan.ExecuteNonQuery();
-                if (nRowsAffected > 0) {
+                int nRowsAffected = DeleteCourseGP.ExecuteNonQuery();
+                if (nRowsAffected > 0)
+                {
                     Message.Visible = true;
                     Message.ForeColor = System.Drawing.Color.Green;
                     Message.Text = "Succesfully inserted Graduation Plan";
@@ -138,11 +140,6 @@ namespace Advising_System
                 Message.Text = ex.Message;
             }
             finally { connection.Close(); }
-        }
-
-        protected void BackAdvisorHome(object sender, EventArgs e)
-        {
-            Response.Redirect("/AdvisorHome.aspx");
         }
     }
 }

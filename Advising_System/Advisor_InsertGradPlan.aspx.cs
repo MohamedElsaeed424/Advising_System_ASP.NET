d15c8c8da35d41e6c924a994a61bd60c19328ce5
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-13 03:01:50 +0200

    insert gradPlan

--- Advising_System/Advisor_InsertGradPlan.aspx.cs
+++ Advising_System/Advisor_InsertGradPlan.aspx.cs
@@ -13,14 +13,21 @@ namespace Advising_System
         {
 
         }
-
-        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
         protected void BackAdvisorHome(object sender, EventArgs e)
         {
             Response.Redirect("/AdvisorHome.aspx");
+        }
+
+        protected void CreateGrad_Click(object sender, EventArgs e)
+        {
+            string semCode = semesterCode.Text;
+            string gradDate = semesterCode.Text;
+            string CH = semCH.Text;
+            string StID = StudentID.Text;
+            int advisorID = 8; // session["UserID"]
+            if(string.IsNullOrEmpty(semCode) || string.IsNullOrEmpty(gradDate)
+                || string.IsNullOrEmpty(CH) || string.IsNullOrEmpty(StID)) { }
+            
         }
     }
 }

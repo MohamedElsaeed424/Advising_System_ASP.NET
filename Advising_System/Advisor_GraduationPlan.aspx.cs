d6a12ba45d261966154200ac896725268a5ec762
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-15 18:32:14 +0200

    commit

---
+++ Advising_System/Advisor_GraduationPlan.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Advising_System
+{
+    public partial class Advisor_GraduationPlan : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BackAdvisorHome(object sender, EventArgs e)
+        {
+            Response.Redirect("/AdvisorHome.aspx");
+        }
+    }
+}

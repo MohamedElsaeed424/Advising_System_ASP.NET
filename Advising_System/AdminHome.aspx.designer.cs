6658f73d57944fa4b1d36d491af8a9e4dfdfd1f9
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-16 17:04:34 +0200

    log out

--- Advising_System/AdminHome.aspx.designer.cs
+++ Advising_System/AdminHome.aspx.designer.cs
@@ -21,15 +21,15 @@ namespace Advising_System
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm registrationForm;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// LogOut control.
+        /// Button1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button LogOut;
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }

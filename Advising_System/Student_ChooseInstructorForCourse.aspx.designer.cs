a7dc65803ea516ad6f885c7f6fc698c29c58d7f0
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-12-16 22:52:17 +0200

    Student choose instructor check if rows were even affected and bugs

--- Advising_System/Student_ChooseInstructorForCourse.aspx.designer.cs
+++ Advising_System/Student_ChooseInstructorForCourse.aspx.designer.cs
@@ -42,13 +42,13 @@ namespace Advising_System
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// TextBox2 control.
+        /// Instructors control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        protected global::System.Web.UI.WebControls.DropDownList Instructors;
 
         /// <summary>
         /// Label3 control.
@@ -60,13 +60,13 @@ namespace Advising_System
         protected global::System.Web.UI.WebControls.Label Label3;
 
         /// <summary>
-        /// TextBox3 control.
+        /// Courses control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        protected global::System.Web.UI.WebControls.DropDownList Courses;
 
         /// <summary>
         /// Label4 control.

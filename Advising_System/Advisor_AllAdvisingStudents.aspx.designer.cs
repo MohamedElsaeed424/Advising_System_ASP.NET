cb6c80dc7451891af5e825bf404bf0330daaf884
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-12-16 14:23:17 +0200

    qweqw

--- Advising_System/Advisor_AllAdvisingStudents.aspx.designer.cs
+++ Advising_System/Advisor_AllAdvisingStudents.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace Advising_System
     {
 
         /// <summary>
-        /// form1 control.
+        /// form control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form;
 
         /// <summary>
         /// BackHome control.
